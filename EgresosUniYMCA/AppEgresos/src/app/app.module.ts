6bde7dd544cf02bc747aa47d17003b535b74128f
JG <j053_pepe@outlook.com>
2017-06-09 12:03:40 -0500

    cambios para Redireccionamiento, se agrego hash #

--- EgresosUniYMCA/AppEgresos/src/app/app.module.ts
+++ EgresosUniYMCA/AppEgresos/src/app/app.module.ts
@@ -1,7 +1,7 @@
 ﻿import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {
@@ -22,43 +22,44 @@ import { HomeComponent } from './component.home/home.component';
 import { AltaDocenteComponent } from './Docente/AltaDocente.component';
 
 @NgModule({
-  declarations: [
-      AppComponent,
-      MenuComponent,
-      HomeComponent,
-      AltaDocenteComponent
-  ],
-  imports: [
-      BrowserModule,
-      HttpModule,
-      CommonModule,
-      FormsModule, 
-      ReactiveFormsModule,
-      ButtonModule,
-      GrowlModule,
-      DataTableModule,
-      SharedModule,
-      DropdownModule,
-      StepsModule,
-      TabViewModule,
-      PanelModule,
-      ChipsModule,
-      CalendarModule,
-      CheckboxModule,
-      FileUploadModule,
-      HttpModule,
-      BrowserAnimationsModule,
-      RouterModule.forRoot([
-          { path: '', redirectTo: 'home', pathMatch: 'full' },
-          { path: 'home', component: HomeComponent },
-          { path: 'AltaDocente', component: AltaDocenteComponent },
-          { path: '**', redirectTo: 'home' }
-      ]),
-      BsDropdownModule.forRoot(),
-  ],
-  exports: [
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        MenuComponent,
+        HomeComponent,
+        AltaDocenteComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpModule,
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        ButtonModule,
+        GrowlModule,
+        DataTableModule,
+        SharedModule,
+        DropdownModule,
+        StepsModule,
+        TabViewModule,
+        PanelModule,
+        ChipsModule,
+        CalendarModule,
+        CheckboxModule,
+        FileUploadModule,
+        HttpModule,
+        BrowserAnimationsModule,
+        RouterModule.forRoot([
+            { path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path: 'home', component: HomeComponent },
+            { path: 'AltaDocente', component: AltaDocenteComponent },
+            { path: '**', redirectTo: 'home' }
+        ], { useHash: true }),
+        BsDropdownModule.forRoot(),
+    ],
+    exports: [
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
