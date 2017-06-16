8896add992d149ffbd0ea54c02da648a9bd617f0
JG <j053_pepe@outlook.com>
2017-06-16 10:31:17 -0500

    primera version mejorada del menu, inclusion de JS y css en el angular Cli

--- EgresosUniYMCA/AppEgresos/src/app/app.component.ts
+++ EgresosUniYMCA/AppEgresos/src/app/app.component.ts
@@ -1,10 +1,14 @@
 ﻿import { Component } from '@angular/core';
-
+const imgprof = './img/default_user.jpg';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
     title = 'app';
+
+    private RutaFoto = imgprof;// require('./img/default_user.jpg');
 }
--- EgresosUniYMCA/AppEgresos/src/app/component.menu/menu.component.ts
+++ EgresosUniYMCA/AppEgresos/src/app/component.menu/menu.component.ts
@@ -8,7 +8,7 @@ import 'rxjs/add/operator/map';
 @Component({
     selector: 'MenuComponent',
     templateUrl: './menu.component.html',
-    styleUrls:['./menu.component.css','../app.component.css']
+    styleUrls:['../app.component.css']
 })
 export class MenuComponent {
     isIn = false;   // store state
@@ -24,16 +24,6 @@ export class MenuComponent {
             .get('api/Menu').subscribe(result => {
                 this.listMenu = result.json();
             });
-       //menu:Observable<MenuP[]>;
      }
-
-    // GetMenu(): Observable<MenuP[]> {
-    //     //window.console.log("Estoy Adentro");
-    //     return 
-    //     this.http
-    //         .get('api/Menu')
-    //         .map(this.CrearMenu)
-    //         .catch(this.handleErrorObservable);                
-    // }
     
 }
